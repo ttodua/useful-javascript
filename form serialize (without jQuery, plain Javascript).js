b1c9c3e05768c2459497465e9315c45a8351e0fb
Tazo Todua <tazotodua@gmail.com>
2014-12-26 16:48:47 +0400

    Create form serialize (without jQuery, plain Javascript).js

---
+++ form serialize (without jQuery, plain Javascript).js
@@ -0,0 +1,24 @@
+//
+function serializeForm(formID) {
+	form = document.getElementById(formID) || document.forms[0];
+	var elems = form.elements;
+	var serialized = [], i, len = elems.length, str='';
+	for(i = 0; i < len; i += 1) {
+	var element = elems[i];
+	var type = element.type;
+	var name = element.name;
+	var value = element.value;
+	switch(type) {
+		case 'text':
+		case 'radio':
+		case 'checkbox':
+		case 'textarea':
+		case 'select-one':
+			str = name + '=' + value;
+			serialized.push(str);break;
+		default:
+			break;
+		}    
+	}
+return serialized.join('&');
+}
