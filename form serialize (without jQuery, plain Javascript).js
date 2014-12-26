2f05205a835301cd14852d1632ddbd5dbeab5fac
Tazo Todua <tazotodua@gmail.com>
2014-12-26 17:11:07 +0400

    Update form serialize (without jQuery, plain Javascript).js

--- form serialize (without jQuery, plain Javascript).js
+++ form serialize (without jQuery, plain Javascript).js
@@ -1,18 +1,1 @@
-EXAMPLE: alert(  serializeForm('MyFormIDD')   ); 
-
-<script type="text/javascript">
-// https://github.com/tazotodua/useful-javascript/
-function serializeForm(formID) {
-	form = document.getElementById(formID) || document.forms[0];
-	var elems = form.elements;
-	var serialized = [], i, len = elems.length, str='';
-	for(i = 0; i < len; i += 1) {
-		switch(elems[i].type) {
-			case 'text': case 'textarea':	case 'select-one': case 'radio': case 'checkbox': 
-			serialized.push(elems[i].name + '=' + elems[i].value); break;
-			default: break;
-		}    
-	}
-	return serialized.join('&');
-}
-</script>
+view at https://code.google.com/p/form-serialize/source/browse/trunk/serialize-0.1.js
