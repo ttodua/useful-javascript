d160066eb442aac286e1bf83034874557de117b4
Tazo Todua <tazotodua@gmail.com>
2014-12-26 17:30:26 +0400

    Update form serialize (without jQuery, plain Javascript).js

--- form serialize (without jQuery, plain Javascript).js
+++ form serialize (without jQuery, plain Javascript).js
@@ -1,4 +1,5 @@
-EXAMPLE: var myData= serializeForm(  document.getElementById('MyFormIDD')  )  ; 
+EXAMPLE:
+serializeForm(  document.getElementById('MyFormIDD')  ); 
 
 
 <script type="text/javascript">          //minified verion of https://code.google.com/p/form-serialize/source/browse/trunk/serialize-0.1.js
