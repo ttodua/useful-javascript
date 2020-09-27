debc268edc5f35a2b2628043108a27f66347bd57
T. Todua <7117978+ttodua@users.noreply.github.com>
2020-09-27 16:32:19 +0400

    Update cookies-library.js

--- cookies-library.js
+++ cookies-library.js
@@ -1,6 +1,6 @@
 /*   
 	##############################################################################################
-	############### my custom solution alike https://github.com/js-cookie/js-cookie  #############
+	############### Extended functionalities of  https://github.com/js-cookie/js-cookie ##########
 	############### https://github.com/ttodua/useful-javascript/edit/master/cookies-library.js ###
 	##############################################################################################
 	
@@ -27,39 +27,12 @@
 	Cookiess.setOptionArray("myCooki", "Joseph",  "Canada",    false);// Remove item from array
 
 */
-var Cookiess = {
-
-	set(name,value,days) {
-		var expires = "";
-		if (days) {
-			var date = new Date();
-			date.setTime(date.getTime() + (days*24*60*60*1000));
-			expires = "; expires=" + date.toUTCString();
-		}
-		document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-	},
-	get(name) {
-		var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-		}
-		return null;
-	},
-	delete(name) {   
-		document.cookie = name+'=; Max-Age=-99999999;';  
-		this.set(key, "", -9999);
-	},
-	append(key, value, attributes) {
-		this.set(key, get(key) + value, attributes);
-	},
-	isSet(cookiename) { 
-		return document.cookie.indexOf('; '+cookiename+'=');
-	}, 
-
-
+Cookiess = {
+	get(a,b) { return this.cookies_instance().get(a,b); },
+	set(a,b,c) { return this.cookies_instance().set(a,b);  },
+	remove(a, b) {return this.cookies_instance().remove(a,b);   },
+	append(name, value, attributes) {return this.cookies_instance().set((this.get(name) || '')  + value, attributes);  },
+	isset(cookiename) {return this.get(cookiename)!="";}, // document.cookie.indexOf('; '+cookiename+'='); 
 	// WORKING WITH ARRAY/OBJECTS
 	getOption(cookieName, key, defaultValue)
 	{ 
@@ -76,10 +49,7 @@ var Cookiess = {
 	{
 		var cookie = this.get(cookieName);
 		var parsed = {};
-		if(cookie)
-		{
-			parsed = JSON.parse(cookie);
-		}
+		if(cookie) parsed = JSON.parse(cookie); 
 		parsed[key] = value;
 		var attributes = attributes ||  { expires: 99999 };
 		this.set(cookieName, JSON.stringify(parsed), attributes);
@@ -89,10 +59,7 @@ var Cookiess = {
 	{
 		var cookie = this.get(cookieName);
 		var parsed = {};
-		if(cookie)
-		{
-			parsed = JSON.parse(cookie);
-		}
+		if(cookie) parsed = JSON.parse(cookie); 
 		if(key in parsed) delete parsed[key];
 		var attributes = attributes ||  { expires: 99999 };
 		this.set(cookieName, JSON.stringify(parsed), attributes);
@@ -120,6 +87,10 @@ var Cookiess = {
 			existing = this.removeItem(existing, subValue);
 		}
 		this.setOption(cookieName, key, JSON.stringify(existing));
-	}
-  
+	},
+	//
+	cookies_instance(){ if (!this.cookies_inited) this.cookies_inited=this.cookies(); return this.cookies_inited; } ,
+	cookies_inited: null,
+	// https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js
+	cookies : function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var r in n)e[r]=n[r]}return e}var t={read:function(e){return e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}};return function n(r,o){function i(t,n,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape),n=r.write(n,t);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n+c}}return Object.create({set:i,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var n=document.cookie?document.cookie.split("; "):[],o={},i=0;i<n.length;i++){var c=n[i].split("="),u=c.slice(1).join("=");'"'===u[0]&&(u=u.slice(1,-1));try{var f=t.read(c[0]);if(o[f]=r.read(u,f),e===f)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){i(t,"",e({},n,{expires:-1}))},withAttributes:function(t){return n(this.converter,e({},this.attributes,t))},withConverter:function(t){return n(e({},this.converter,t),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(r)}})}(t,{path:"/"})}
 };
